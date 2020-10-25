5b37552a0ed28aceb18ef19d7f30ad9b0e7aed83
zmx <zmx@163.com>
2020-10-25 23:26:29 +0800

    代码修改

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,27 +1,45 @@
 <template>
   <div>
-    <detail-nav-bar></detail-nav-bar>
+    <detail-nav-bar/>
+    <detail-swiper :topImages="topImages"/>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar'
+  import DetailSwiper from './childComps/DetailSwiper'
+
+  import {getDetailData} from 'network/detail'
   export default {
     name: "Detail",
     data() {
       return {
-        iid: null
+        iid: null,
+        topImages: []
       }
     },
     components: {
-      DetailNavBar
+      DetailNavBar,
+      DetailSwiper
     },
     created() {
+      // 1.将动态路由中的iid进行保存(因为他是一个局部变量，我们必须将它进行保存)
       this.iid = this.$route.params.iid
+
+      // 2.根据iid请求不同商品的数据
+      getDetailData(this.iid).then((res) => {
+        // 1.获取顶部轮播图的图片数据
+        console.log(res);
+        const data = res.result
+        this.topImages = data.itemInfo.topImages
+      }).catch((err) => {
+        
+      });
     }
   }
 
 </script>
 
 <style scoped>
+  
 </style>
